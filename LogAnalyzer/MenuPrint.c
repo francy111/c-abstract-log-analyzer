a8630b1cf6a701a158332d8f95fced180bad4a28
francy111 <francesco.biribo.m01@gmail.com>
2025-02-08 00:59:50 +0100

    Added copyright comments

--- LogAnalyzer/MenuPrint.c
+++ LogAnalyzer/MenuPrint.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024 Biribo' Francesco
+ *
+ * Permission to use, copy, modify, and distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include "MenuPrint.h"
 #include "DLinkedList.h"
 #include <stdio.h>
@@ -191,7 +199,7 @@ void logAnalysisMenu(enum analysis_operation operation, DLinkedList* userFilters
 		atLeastOne = 1;
 	}
 	if (operationFilters != NULL) {
-		printf("\n user in " BOLD MAGENTA);
+		printf("\n operation in " BOLD MAGENTA);
 		char* bff = listToString(operationFilters, HEAD_TO_TAIL);
 		printf("%s" RESET, bff);
 		free(bff);
